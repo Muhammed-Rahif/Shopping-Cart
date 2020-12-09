596a379c5de17446b14602a0d663a885c7cb50d2
Muhammed Rahif <rahifpalliyalil@gmail.com>
2020-12-09 16:57:01 +0530

    Bootstrap set up finished,Card view design done,Arrey created for products,express default layouts and partials set up finished...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {
+      name:"I phone 11",
+      catogory:"Mobile",
+      discription:"This is I phone",
+      price:"60000",
+      image:"https://images-na.ssl-images-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+    },
+    {
+      name:"Realme",
+      catogory:"Mobile",
+      discription:"This is Realme",
+      price:"60000",
+      image:"https://images-na.ssl-images-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+    },
+    {
+      name:"Samsung",
+      catogory:"Mobile",
+      discription:"This is Samsung",
+      price:"60000",
+      image:"https://images-na.ssl-images-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+    },
+    {
+      name:"Lonevo",
+      catogory:"Mobile",
+      discription:"This is Lonevo",
+      price:"60000",
+      image:"https://images-na.ssl-images-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg"
+    }
+  ]
+  res.render('index', { products,admin:true });
 });
 
 module.exports = router;
