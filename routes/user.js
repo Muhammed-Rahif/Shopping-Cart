1c5d324647062764e7cf3eddf8cda19b2794558d
Muhammed Rahif <rahifpalliyalil@gmail.com>
2020-12-10 18:02:16 +0530

    User login valid or invalid checking done,cart page added for logged in users,Admin delete product button action done with confirmation question...

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
 var userHelpers = require('../helpers/user-helpers')
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   let user=req.session.user
@@ -11,7 +18,11 @@ router.get('/', function (req, res, next) {
   })
 });
 router.get('/login',(req,res)=>{
-  res.render('user/login-page')
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+    res.render('user/login-page',{"loginErr":req.session.loginErr})
+  }
 })
 router.get('/signup',(req,res)=>{
   res.render('user/signup-page')
@@ -28,6 +39,7 @@ router.post('/login',(req,res)=>{
       req.session.user=response.user
       res.redirect('/')
     }else{
+      req.session.loginErr="Invalid email or password"
       res.redirect('/login')
     }
   })
@@ -35,6 +47,10 @@ router.post('/login',(req,res)=>{
 router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
+})
+router.get('/cart',verifyLogin,(req,res)=>{
+  let user=req.session.user
+  res.render('user/cart',{user})
 })
 
 module.exports = router ;
