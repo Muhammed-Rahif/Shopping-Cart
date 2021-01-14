74f73b75fb4d530c229b8a091d09976f58fa48d6
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-14 16:05:40 +0530

    Some problems fixed, Product image file will remove when product delete in admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
+var fs = require('fs');
 
 const verifyLogin = (req, res, next) => {
   if (req.session.admin) {
@@ -10,14 +11,14 @@ const verifyLogin = (req, res, next) => {
   }
 }
 /* GET home page. */
-router.get('/',verifyLogin, function (req, res, next) {
+router.get('/', verifyLogin, function (req, res, next) {
   productHelpers.getAllProducts().then((products) => {
-    let admin=req.session.admin
+    let admin = req.session.admin
     res.render('admin/view-products', { products, admin });
   })
 });
-router.get('/add-product',verifyLogin, (req, res) => {
-  let admin=req.session.admin
+router.get('/add-product', verifyLogin, (req, res) => {
+  let admin = req.session.admin
   res.render('admin/add-product', { admin })
 })
 router.post('/add-product', (req, res) => {
@@ -25,22 +26,30 @@ router.post('/add-product', (req, res) => {
     let image = req.files.image
     image.mv('./public/product-images/' + id + '.jpg', (err, done) => {
       if (!err) {
-        let admin=req.session.admin
-        res.render('admin/add-product', { admin })
+        let admin = req.session.admin
+        res.render('admin/add-product', { admin, proAdded: "Product added successfully..!" })
       } else {
         console.log(err);
       }
     })
   })
 })
-router.get('/delete-product/:id',verifyLogin, (req, res) => {
+router.get('/delete-product/:id', verifyLogin, (req, res) => {
   let proId = req.params.id;
+
+  // deleting image file
+  fs.unlink('./public/product-images/' + proId + '.jpg', function (err) {
+    if (err) throw err;
+    console.log('File deleted!');
+  });
+
   productHelpers.deleteProduct(proId).then((response) => {
     res.redirect('/admin/')
   })
 })
-router.get('/edit-product/:id',verifyLogin, (req, res) => {
+router.get('/edit-product/:id', verifyLogin, (req, res) => {
   productHelpers.getProductDetails(req.params.id).then((product) => {
+    let admin = req.session.admin;
     res.render('admin/edit-product', { admin, product })
   })
 })
@@ -57,7 +66,7 @@ router.get('/login', (req, res) => {
   if (req.session.adminLoggedIn) {
     res.redirect('/admin')
   } else {
-    res.render('admin/login-page', { "loginErr": req.session.adminLoginErr, admin:true})
+    res.render('admin/login-page', { "loginErr": req.session.adminLoginErr, admin: true })
     req.session.adminLoginErr = false
   }
 })
@@ -74,34 +83,34 @@ router.post('/login', (req, res) => {
   })
 })
 router.get('/logout', (req, res) => {
-  req.session.admin=null
-  req.session.adminLoggedIn=false
+  req.session.admin = null
+  req.session.adminLoggedIn = false
   res.redirect('/admin')
 })
-router.get('/all-orders',verifyLogin,(req,res)=>{
-  let admin=req.session.admin;
-  productHelpers.getAllOrders().then((orders)=>{
+router.get('/all-orders', verifyLogin, (req, res) => {
+  let admin = req.session.admin;
+  productHelpers.getAllOrders().then((orders) => {
     console.log(orders);
-    res.render('admin/all-orders',{ admin ,orders})
+    res.render('admin/all-orders', { admin, orders })
   })
 })
-router.post('/change-order-status',(req,res)=>{
+router.post('/change-order-status', (req, res) => {
   console.log(req.body);
-  productHelpers.changeOrderStatus(req.body.orderId,req.body.orderStatus).then((orderStatus)=>{
+  productHelpers.changeOrderStatus(req.body.orderId, req.body.orderStatus).then((orderStatus) => {
     console.log(orderStatus);
-    res.json({orderStatus})
+    res.json({ orderStatus })
   })
 })
-router.get('/all-users',verifyLogin,async(req,res)=>{
-  let admin=req.session.admin;
-  let users=await productHelpers.getAllUsers()
-  res.render('admin/all-users',{ users , admin})
+router.get('/all-users', verifyLogin, async (req, res) => {
+  let admin = req.session.admin;
+  let users = await productHelpers.getAllUsers()
+  res.render('admin/all-users', { users, admin })
 })
-router.get('/view-user-orders/:id',verifyLogin,async(req,res)=>{
-  let admin=req.session.admin;
-  let userOrders=await productHelpers.getUserOrders(req.params.id)
-  let userData=await productHelpers.getUserData(req.params.id)
-  res.render('admin/user-orders',{admin,userOrders,userData})
+router.get('/view-user-orders/:id', verifyLogin, async (req, res) => {
+  let admin = req.session.admin;
+  let userOrders = await productHelpers.getUserOrders(req.params.id)
+  let userData = await productHelpers.getUserData(req.params.id)
+  res.render('admin/user-orders', { admin, userOrders, userData })
 })
 
 
